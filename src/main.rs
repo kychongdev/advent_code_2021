24d26f2951f11e02673a2d2d8b671083011cf5c0
kychongdev <kychongdev@gmail.com>
2021-12-04 17:07:07 +0800

    move part1 solution to function

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ mod part1;
 use crate::part1::read_line_from_file;
 
 fn main() {
+    part1_solution();
+}
+
+fn part1_solution() {
     let file = read_line_from_file("./src/part1.txt");
     let mut first_number = &file[0];
     let mut count = 0;
