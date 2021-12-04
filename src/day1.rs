b19671cb889c0d9b0cfe04f7bd9e6b71d149016e
kychongdev <kychongdev@gmail.com>
2021-12-04 22:25:12 +0800

    move day 1 fn out to day1.rs

--- src/day1.rs
+++ src/day1.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use std::path::Path;
 
-pub fn read_line_from_file(filename: impl AsRef<Path>) -> Vec<i32> {
+fn read_line_from_file(filename: impl AsRef<Path>) -> Vec<i32> {
     let file = File::open(filename).expect("No such file");
     let buf = BufReader::new(file);
     buf.lines()
@@ -12,4 +12,35 @@ pub fn read_line_from_file(filename: impl AsRef<Path>) -> Vec<i32> {
         .filter(|item| item.is_ok())
         .map(|item| item.unwrap())
         .collect()
+}
+
+pub fn day1_part1() {
+    let file = read_line_from_file("./src/day1.txt");
+    let mut first_number = &file[0];
+    let mut count = 0;
+    for line in file.iter().skip(1) {
+        if line > first_number {
+            count += 1;
+        }
+        first_number = line;
+    }
+
+    println!("Day 1 part 1 answer: {}", count);
+}
+
+pub fn day1_part2() {
+    let file = read_line_from_file("./src/day1.txt");
+
+    let mut count = 0;
+    for (index, line) in file.iter().enumerate() {
+        if index + 3 > file.len() - 1 {
+            break;
+        }
+
+        if line < &file[index + 3] {
+            count += 1;
+        }
+    }
+
+    println!("Day 1 part 2 answer: {}", count);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,8 @@
 mod day1;
 
-use crate::day1::read_line_from_file;
+use crate::day1::{day1_part1, day1_part2};
 
 fn main() {
-    // part1_solution();
+    day1_part1();
     day1_part2();
-}
-
-fn day1_part1() {
-    let file = read_line_from_file("./src/day1.txt");
-    let mut first_number = &file[0];
-    let mut count = 0;
-    for line in file.iter().skip(1) {
-        if line > first_number {
-            count += 1;
-        }
-        first_number = line;
-    }
-
-    println!("{}", count);
-}
-
-fn day1_part2() {
-    let file = read_line_from_file("./src/day1.txt");
-
-    let mut count = 0;
-    for (index, line) in file.iter().enumerate() {
-        if index + 3 > file.len() - 1 {
-            break;
-        }
-
-        if line < &file[index + 3] {
-            count += 1;
-        }
-    }
-
-    println!("{}", count);
 }
